750aa0ac0b47757bc03f0cc55cf4c1f88d89cff1
A1tBot <timofeyershovv@gmail.com>
2025-05-11 19:06:25 +0700

    Компонент с характеристиками выбранного товара (Характеристики выбранного товара)

---
+++ src/components/selected_product/ProductParam.jsx
@@ -0,0 +1,111 @@
+export const ProductParam = () => {
+    return (
+        <div className="productParamWrapper">
+            <div className="productParamName">
+                <h3>Параметры</h3>
+            </div>
+
+            <div className="productParamContainer">
+                <div className="productParamItem">
+                    <p>Гарантия продавца</p>
+                    <p>12 мес.</p>
+                </div>
+
+                <div className="productParamItem">
+                    <p>Страна-производитель</p>
+                    <p>Китай</p>
+                </div>
+
+                <div className="productParamItem">
+                    <p>Тип</p>
+                    <p>Планшет</p>
+                </div>
+
+                <div className="productParamItem">
+                    <p>Бренд</p>
+                    <p>Apple</p>
+                </div>
+
+                <div className="productParamItem">
+                    <p>Модель</p>
+                    <p>iPhone 15 Pro max</p>
+                </div>
+
+                <div className="productParamItem">
+                    <p>Цвет</p>
+                    <p>Серый</p>
+                </div>
+
+                <div className="productParamItem">
+                    <p>Операционная система</p>
+                    <p>iOS</p>
+                </div>
+
+                <div className="productParamItem">
+                    <p>Модель процессора</p>
+                    <p>MediaTek Helio g99</p>
+                </div>
+
+                <div className="productParamItem">
+                    <p>Количество ядер</p>
+                    <p>8</p>
+                </div>
+
+                <div className="productParamItem">
+                    <p>Объем опреативной памяти</p>
+                    <p>12 ГБ</p>
+                </div>
+                
+                <div className="productParamItem">
+                    <p>Объем встроенной памяти</p>
+                    <p>256 ГБ</p>
+                </div>
+
+                <div className="productParamItem">
+                    <p>Количество камер</p>
+                    <p>1</p>
+                </div>
+
+                <div className="productParamItem">
+                    <p>Количество мегапикселей</p>
+                    <p>45 Мп</p>
+                </div>
+
+                <div className="productParamItem">
+                    <p>Материал корпуса</p>
+                    <p>Металл</p>
+                </div>
+
+                <div className="productParamItem">
+                    <p>Интерфейс</p>
+                    <p>USB Type-C</p>
+                </div>
+
+                <div className="productParamItem">
+                    <p>Емкость аккумулятор</p>
+                    <p>3000 мА * ч</p>
+                </div>
+
+                <div className="productParamItem">
+                    <p>Ширина</p>
+                    <p>77.6 мм</p>
+                </div>
+
+                <div className="productParamItem">
+                    <p>Высота</p>
+                    <p>161.5 мм</p>
+                </div>
+
+                <div className="productParamItem">
+                    <p>Толщина</p>
+                    <p>15 мм.</p>
+                </div>
+
+                <div className="productParamItem">
+                    <p>Вес</p>
+                    <p>150 г</p>
+                </div>
+            </div>
+        </div>
+    )
+}
