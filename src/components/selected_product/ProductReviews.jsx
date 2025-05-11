0b29143b7ab6cce87821fa7ccecee1b695e3eac3
A1tBot <timofeyershovv@gmail.com>
2025-05-11 19:06:44 +0700

    Копонент с отзывами (пока хз, не хочу его делать, он пустой)

---
+++ src/components/selected_product/ProductReviews.jsx
@@ -0,0 +1,9 @@
+export const ProductReviews = () => {
+    return (
+        <div className="productReviewsWrapper">
+            <div className="productReviewsName">
+                <h3>Отзывы</h3>
+            </div>
+        </div>
+    )
+}
