161eff3677f42c8560b6d2d9426c5a368fc5789b
A1tBot <timofeyershovv@gmail.com>
2025-05-12 17:43:20 +0700

    Корзина

--- src/components/cart/CartPage.jsx
+++ src/components/cart/CartPage.jsx
@@ -1,7 +1,38 @@
+import "../../styles/cart.css"
+
+import { useEffect, useState } from "react"
+import { useLocation } from "react-router-dom"
+
+import { Breadcrumbs } from "../Breadcrumbs"
+import { FooterComponent } from "../footer/FooterComponent"
+import { HeaderComponent } from "../header/HeaderComponent"
+import { CartItemsGrid } from "./CartItemsGrid"
+
 export const CartPage = () => {
+    const location = useLocation()
+    const [breadcrumbItems, setBreadcrumbItems] = useState([{ label: "Главная", path: "/" }])
+    
+    useEffect(() => {
+        setBreadcrumbItems([{ label: "Главная", path: "/" }])
+    }, [location.pathname])
+
+    useEffect(() => {
+        if (location.state) {
+            setBreadcrumbItems(prev => [
+                { label: "Главная", path: "/" },
+                { label: location.state, path: "" }
+            ])
+        }
+    }, [location.state])
+
     return (
-        <div>
-            <p>Корзина</p>
-        </div>
+        <>
+            <HeaderComponent/>
+            <div className="contentWrapper">
+                <Breadcrumbs items={breadcrumbItems}/>
+                <CartItemsGrid/>
+            </div>
+            <FooterComponent/>
+        </>
     )
 }
