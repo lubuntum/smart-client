c2be7fb9a47ae81567935e02e4356c1ca7002413
A1tBot <timofeyershovv@gmail.com>
2025-05-12 17:43:36 +0700

    Компонент с итоговыми ценами

---
+++ src/components/cart/CartPrice.jsx
@@ -0,0 +1,18 @@
+export const CartPrice = () => {
+    return (
+        <div className="cartPriceContainer">
+            <div className="cartPriceItem">
+                <p>Сумма:</p>
+                <p>7 068 р.</p>
+            </div>
+            <div className="cartPriceItem">
+                <p>В том числе НДС 10%:</p>
+                <p>648 р.</p>
+            </div>
+            <div className="cartPriceItem">
+                <p>Итого:</p>
+                <p>7 068 р.</p>
+            </div>
+        </div>
+    )
+}
